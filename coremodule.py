65e0a887c36e420ad39b4e72e513b44d86894f08
Dragos-Marian <48670899+Dragos-Marian@users.noreply.github.com>
2020-08-27 12:52:18 +0300

    coremodule +plotly added

--- coremodule.py
+++ coremodule.py
@@ -7,6 +7,8 @@ import logging
 import sys
 import database as db
 from firebase import firebase
+import plotly.graph_objs as go
+import plotly.offline as ply
 
 
 parser = argparse.ArgumentParser()
@@ -30,6 +32,8 @@ TEMP_UNDER_THRESHOLD: int = 0
 TEMP_OVER_THRESHOLD: int = 0
 MAX_END_FEVER: int = 9
 SLEEP_DURATION: int = args.sleep
+X: int=[]
+Y: int=[]
 
 firebase=firebase.FirebaseApplication("https://iotproject-45a41.firebaseio.com/",None)
 
@@ -61,6 +65,33 @@ def get_fever_event(temperature: float):
     
     return None
 
+def write_in_plotly(temp_under_threshold,temperature,time):
+     global X
+     global Y
+     global data
+     global fig
+     if temp_under_threshold != MAX_END_FEVER:
+          X.append(time)
+          Y.append(temperature)
+          data=[go.Scatter(
+               x = X,
+               y = Y,
+               name = "Temperature by Time",
+               line = dict(
+                    color = ("green"),
+                    width = 4,
+                    dash = 'dashdot'
+               )
+           )]
+          layout = dict(
+             title = "Temperature readings until Fever_End",
+             xaxis = dict(title = "Time"),
+             yaxis = dict(title = "Temperature")
+          )
+          fig = dict(data = data,layout = layout)
+     if temp_under_threshold == MAX_END_FEVER:
+          ply.plot(fig, filename='test.html')
+
 
 def get_current_time():
     return int(time.time())
@@ -88,6 +119,7 @@ def main():
         current_time = get_current_time()
 
         send_to_firebase(fever_event,current_time)
+        write_in_plotly(TEMP_UNDER_THRESHOLD,temperature,current_time)
         with conn:
             db.insert_row(conn, current_time, temperature, fever_event, db.TABLE_NAME)
 
